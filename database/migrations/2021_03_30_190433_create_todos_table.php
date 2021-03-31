6b97bdf3373b37d8ec15044550301793b795c834
Youta Obby Sidane <dyosby237@gmail.com>
2021-03-31 07:08:50 +0100

    Fix text size for description column in todos table

--- database/migrations/2021_03_30_190433_create_todos_table.php
+++ database/migrations/2021_03_30_190433_create_todos_table.php
@@ -16,7 +16,7 @@ class CreateTodosTable extends Migration
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->longText('description');
+            $table->text('description');
             $table->boolean('done')->default(0);
             $table->foreignId('user_id');
             $table->timestamps();
